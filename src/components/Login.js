dd928beb1d30e09396d41d72b09b27d09d9baba0
sakethwebdev <152254624+sakethwebdev@users.noreply.github.com>
2024-03-08 17:30:08 +0000

    commitr

--- src/components/Login.js
+++ src/components/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container"  >
+    <div className="login-container" class="requied" >
       <form className="login-form" >
         <h1 >Login</h1>
         <label>Email:</label>
