456c8652544f21f375354b1be61e6052fc59e585
sakethwebdev <152254624+sakethwebdev@users.noreply.github.com>
2024-05-03 18:23:32 +0000

    commit

--- src/components/actions.js
+++ src/components/actions.js
@@ -3,8 +3,6 @@ export const incrementQuantity = (itemId) => ({
     type: 'INCREMENT_QUANTITY',
     payload: itemId,
   });
-  
-  
   export const decrementQuantity = (itemId) => ({
     type: 'DECREMENT_QUANTITY',
     payload: itemId,
